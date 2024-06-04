00f32d994cfca166ff998924a07999de871bf6f7
Cristiano Virginio <cristianovriginio@gmail.com>
2024-06-03 22:08:34 -0300

    uso do metodo SORT

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,13 +1,9 @@
-//const btnFiltrarLivros = document.getElementById("btnFiltrarLivrosFront");
-//btnFiltrarLivros.addEventListener("click", FiltrarLivrosFront)
 const botoes = document.querySelectorAll('.btn')
-botoes.forEach(btn => btn.addEventListener("click",filtrarLivros))
-
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro =>livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
-
 }
